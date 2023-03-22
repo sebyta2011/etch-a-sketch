a9c7f10454e5e483cca6794c02e6cdb333ba6794
Sebastian <sebytanuniez@gmail.com>
2023-03-22 20:56:44 -0300

    Added button that resets the grid and asks for another grid size; deleted residual comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const contenedor = document.querySelector("#contenedor");
 const crearDiv = document.createElement('div');
 
-let i = 20;
-function linea() {
+let i = prompt('Desired grid size?');
+function grid() {
     for(a=0; a < i; a++) {
         const crearContenedor = document.createElement("div");
         crearContenedor.classList.add('fila');
@@ -19,25 +19,28 @@ function colorBlack(e) {
     e.target.style.backgroundColor = "black";
 }
 
-linea();
+grid();
 
 const gridTiles = document.querySelectorAll(".tile");
-
 gridTiles.forEach(tile => tile.addEventListener('mouseover', colorBlack));
 
+const resizeButton = document.querySelector("#resizeButton");
+resizeButton.addEventListener('click', function newGrid() {
+    let i = prompt("Desired grid size?");
+    const contenedor = document.querySelector("#contenedor");
+    document.querySelectorAll(".fila").forEach(fila => fila.remove());
+    for(a=0; a < i; a++) {
+        const crearContenedor = document.createElement("div");
+        crearContenedor.classList.add('fila');
+        contenedor.appendChild(crearContenedor);
+            for(b=0; b < i; b++) {
+                const crearDiv = document.createElement('div')
+                crearDiv.classList.add('tile')
+                crearContenedor.appendChild(crearDiv)
+            }
+    }
+    const gridTiles = document.querySelectorAll(".tile");
+    gridTiles.forEach(tile => tile.addEventListener('mouseover', colorBlack));
+});
 
 
-// tengo: 
-// una funcion para crear una linea de cuadraditos dado i linea(i)
-//     necesito: que se cree un contenedor flex-direction row para tener a los cuadraditos creados por la funcion
-// una funcion para crear una columna de cuadraditos dado i columna(i)
-//     necesito: que se cree un contenedor flex-direction column para tener a los cuadraditos creados por la funcion
-
-// quiero:
-// linea(i) loopee i cantidad de veces, y en cada loop tiene que llamar a columna(i) para que loopee i-1 veces
-
-// function linea() {
-//     for(i=0; i<16; ++i) {
-//         const gridTileCreate = document.createElement('div');
-//         const gridRowCreate
-//     }}
